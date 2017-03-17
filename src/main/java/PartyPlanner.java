2668a3a24df523d3b169df0d24fc374bdb3d51fd
Cameron Jensen <jensenseanc@gmail.com>
2017-03-17 09:30:42 -0700

    Add instantiate test for partyplanner. refactoring how to create this app. comment out previous tests and methods.

--- src/main/java/PartyPlanner.java
+++ src/main/java/PartyPlanner.java
@@ -1,23 +1,41 @@
 class PartyPlanner {
+  private int mPartySize;
+  private String mFood;
+  private String mBeverages;
+  private String mEntertainment;
+  private String mDecorations;
 
-  public PartyPlanner() {
 
+
+  public PartyPlanner(int guests, String food, String beverages, String entertainment, String decor) {
+    mPartySize = guests;
+    mFood = food;
+    mBeverages = beverages;
+    mEntertainment = entertainment;
+    mDecorations = decor;
   }
 
-  public int partySize(int guests){
-    if (guests <= 35) {
-      return 50;
-    } else if (guests > 35 && guests <= 70) {
-      return 75;
-    } else if (guests > 70 && guests <= 125) {
-      return 100;
-    } else if (guests > 125 && guests <= 200) {
-      return 150;
-    } else if (guests > 200 && guests <= 300) {
-      return 200;
-    } else if (guests > 300 && guests <= 450) {
-      return 250;
-    }
-    return 0;
-  }
+  // public int partySize(int guests){
+  //   if (guests <= 35) {
+  //     return 50;
+  //   } else if (guests > 35 && guests <= 70) {
+  //     return 75;
+  //   } else if (guests > 70 && guests <= 125) {
+  //     return 100;
+  //   } else if (guests > 125 && guests <= 200) {
+  //     return 150;
+  //   } else if (guests > 200 && guests <= 300) {
+  //     return 200;
+  //   } else if (guests > 300 && guests <= 450) {
+  //     return 250;
+  //   }
+  //   return 0;
+  // }
+  //
+  // public int foodType(String food){
+  //   if (food.equals("snack")){
+  //     return 100;
+  //   }
+  //   return 0;
+  // }
 }
--- src/test/java/PartyPlannerTest.java
+++ src/test/java/PartyPlannerTest.java
@@ -3,12 +3,26 @@ import static org.junit.Assert.*;
 
 public class PartyPlannerTest {
 
-    @Test
-    public void PartPlanner_partyOfTenReturnsPrice_50() {
-      PartyPlanner testPartyPlanner = new PartyPlanner();
-      assertEquals(50, testPartyPlanner.partySize(10));
-      assertEquals(250, testPartyPlanner.partySize(375));
+
+  @Test
+    public void PartyPlanner_instantiatesCorrectly(){
+      PartyPlanner testPartyPlanner = new PartyPlanner(10, "snacks", "light bar", "clown", "birthday");
+      assertEquals(true, testPartyPlanner instanceof PartyPlanner);
     }
+
+    
+    // @Test
+    // public void PartyPlanner_partyOfTenReturnsPrice_50() {
+    //   PartyPlanner testPartyPlanner = new PartyPlanner();
+    //   assertEquals(50, testPartyPlanner.partySize(10));
+    //   assertEquals(250, testPartyPlanner.partySize(375));
+    // }
+    //
+    // @Test
+    // public void PartyPlanner_lightSnackReturnsPrice_100() {
+    //   PartyPlanner testPartyPlanner = new PartyPlanner();
+    //   assertEquals(100, testPartyPlanner.foodType("snack"));
+    // }
 
 
 }
