a5f749571546170bba56bcfbf25d2bb6c1a228a2
Cameron Jensen <jensenseanc@gmail.com>
2017-03-17 11:00:46 -0700

    Add dinner to food options and premium bar to beverage option.

--- src/main/java/PartyPlanner.java
+++ src/main/java/PartyPlanner.java
@@ -37,48 +37,92 @@ class PartyPlanner {
 
   public int foodPrice() {
     int total = 0;
-    if (mPartySize <= 35) {
-      total = mPartySize * 10;
+    if (mFood.equals("snacks")){
+      if (mPartySize <= 35) {
+        total = mPartySize * 10;
+        return total;
+      } else if (mPartySize > 35 && mPartySize <= 70) {
+        total = mPartySize * 9;
+        return total;
+      } else if (mPartySize > 70 && mPartySize <= 125) {
+        total = mPartySize * 8;
+        return total;
+      } else if (mPartySize > 125 && mPartySize <= 200) {
+        total = mPartySize * 7;
+        return total;
+      } else if (mPartySize > 200 && mPartySize <= 300) {
+        total = mPartySize * 6;
+        return total;
+      } else if (mPartySize > 300 && mPartySize <= 450) {
+        total = mPartySize * 5;
+      }
       return total;
-    } else if (mPartySize > 35 && mPartySize <= 70) {
-      total = mPartySize * 9;
-      return total;
-    } else if (mPartySize > 70 && mPartySize <= 125) {
-      total = mPartySize * 8;
-      return total;
-    } else if (mPartySize > 125 && mPartySize <= 200) {
-      total = mPartySize * 7;
-      return total;
-    } else if (mPartySize > 200 && mPartySize <= 300) {
-      total = mPartySize * 6;
-      return total;
-    } else if (mPartySize > 300 && mPartySize <= 450) {
-      total = mPartySize * 5;
-      return total;
+    } else if (mFood.equals("dinner")) {
+      if (mPartySize <= 35) {
+        total = mPartySize * 40;
+        return total;
+      } else if (mPartySize > 35 && mPartySize <= 70) {
+        total = mPartySize * 39;
+        return total;
+      } else if (mPartySize > 70 && mPartySize <= 125) {
+        total = mPartySize * 38;
+        return total;
+      } else if (mPartySize > 125 && mPartySize <= 200) {
+        total = mPartySize * 37;
+        return total;
+      } else if (mPartySize > 200 && mPartySize <= 300) {
+        total = mPartySize * 36;
+        return total;
+      } else if (mPartySize > 300 && mPartySize <= 450) {
+        total = mPartySize * 35;
+        return total;
+      }
     }
     return total;
   }
 
   public int beveragePrice() {
     int total = 0;
-    if (mPartySize <= 35) {
-      total = mPartySize * 15;
-      return total;
-    } else if (mPartySize > 35 && mPartySize <= 70) {
-      total = mPartySize * 14;
-      return total;
-    } else if (mPartySize > 70 && mPartySize <= 125) {
-      total = mPartySize * 13;
-      return total;
-    } else if (mPartySize > 125 && mPartySize <= 200) {
-      total = mPartySize * 10;
-      return total;
-    } else if (mPartySize > 200 && mPartySize <= 300) {
-      total = mPartySize * 9;
-      return total;
-    } else if (mPartySize > 300 && mPartySize <= 450) {
-      total = mPartySize * 7;
-      return total;
+    if (mBeverages.equals("light bar")){
+      if (mPartySize <= 35) {
+        total = mPartySize * 15;
+        return total;
+      } else if (mPartySize > 35 && mPartySize <= 70) {
+        total = mPartySize * 14;
+        return total;
+      } else if (mPartySize > 70 && mPartySize <= 125) {
+        total = mPartySize * 13;
+        return total;
+      } else if (mPartySize > 125 && mPartySize <= 200) {
+        total = mPartySize * 10;
+        return total;
+      } else if (mPartySize > 200 && mPartySize <= 300) {
+        total = mPartySize * 9;
+        return total;
+      } else if (mPartySize > 300 && mPartySize <= 450) {
+        total = mPartySize * 7;
+        return total;
+      }
+    } else if (mBeverages.equals("premium bar")) {
+      if (mPartySize <= 35) {
+        total = mPartySize * 25;
+        return total;
+      } else if (mPartySize > 35 && mPartySize <= 70) {
+        total = mPartySize * 24;
+        return total;
+      } else if (mPartySize > 70 && mPartySize <= 125) {
+        total = mPartySize * 23;
+        return total;
+      } else if (mPartySize > 125 && mPartySize <= 200) {
+        total = mPartySize * 20;
+        return total;
+      } else if (mPartySize > 200 && mPartySize <= 300) {
+        total = mPartySize * 19;
+        return total;
+      } else if (mPartySize > 300 && mPartySize <= 450) {
+        total = mPartySize * 17;
+        return total;
+      }
     }
     return total;
   }
@@ -106,7 +150,7 @@ class PartyPlanner {
   }
 
   public int totalPrice() {
-    int total = foodPrice() + beveragePrice() + entertainmentPrice() + decorationPrice(); 
+    int total = foodPrice() + beveragePrice() + entertainmentPrice() + decorationPrice();
     return total;
   }
 
