74ae3fbf0dedd24c578fe3354b0489886255ee97
Cameron Jensen <jensenseanc@gmail.com>
2017-03-17 15:08:28 -0700

    Add option to view promotions from menu in UI, in UI updated menu to be in number list.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,7 +19,12 @@ public class App {
 
     while (programRun) {
 
-      System.out.println("Welcome to the best party planning ever! What would you like to do? If you would like to look at existing plans enter: plan, if you would like to create your own plan enter: create, if you would like to enter a coupon/discount code enter: discount, if you would like to leave enter: exit.");
+      System.out.println("Welcome to the best party planning ever! What would you like to do?");
+      System.out.println("1. If you would like to look at existing plans enter: plan");
+      System.out.println("2. If you would like to create your own plan enter: create");
+      System.out.println("3. If you would like to enter a coupon/discount code enter: discount");
+      System.out.println("4. If you would like to see current promotions enter: promotions");
+      System.out.println("5. If you would like to leave enter: exit.");
       String userInput = console.readLine();
 
 
@@ -56,9 +61,77 @@ public class App {
           System.out.println("Entertainment Type: " + userPlan.getEntertainmentType());
           System.out.println("Decorations Type: " + userPlan.getDecorationsType());
           System.out.println("Total Price: " + userPlan.totalPrice());
-          System.out.println(" ");
+        System.out.println("<><><><><><><><><><>");
       } else if(userInput.equals("discount")){
-
+        System.out.println("Enter discount code:");
+        String userCoupon = console.readLine();
+        if (userCoupon.equals("birthday")){
+          for (PartyPlanner plan : allPlans ) {
+            System.out.println("<><><><><><><><><><>");
+            System.out.println("Party Size: " + plan.getPartySize());
+            System.out.println("Food Type: " + plan.getFoodType());
+            System.out.println("Beverage Type: " + plan.getBeverageType());
+            System.out.println("Entertainment Type: " + plan.getEntertainmentType());
+            System.out.println("Decorations Type: " + plan.getDecorationsType());
+            System.out.println("Total Price: " + plan.birthdayPackage());
+            System.out.println(" ");
+          }
+        } else if (userCoupon.equals("wedding")) {
+          for (PartyPlanner plan : allPlans ) {
+            System.out.println("<><><><><><><><><><>");
+            System.out.println("Party Size: " + plan.getPartySize());
+            System.out.println("Food Type: " + plan.getFoodType());
+            System.out.println("Beverage Type: " + plan.getBeverageType());
+            System.out.println("Entertainment Type: " + plan.getEntertainmentType());
+            System.out.println("Decorations Type: " + plan.getDecorationsType());
+            System.out.println("Total Price: " + plan.weddingPackage());
+            System.out.println(" ");
+          }
+        } else if (userCoupon.equals("sport")){
+          for (PartyPlanner plan : allPlans ) {
+            System.out.println("<><><><><><><><><><>");
+            System.out.println("Party Size: " + plan.getPartySize());
+            System.out.println("Food Type: " + plan.getFoodType());
+            System.out.println("Beverage Type: " + plan.getBeverageType());
+            System.out.println("Entertainment Type: " + plan.getEntertainmentType());
+            System.out.println("Decorations Type: " + plan.getDecorationsType());
+            System.out.println("Total Price: " + plan.sportPackage());
+            System.out.println(" ");
+          }
+        } else if (userCoupon.equals("fivefive0")){
+          for (PartyPlanner plan : allPlans ) {
+            System.out.println("<><><><><><><><><><>");
+            System.out.println("Party Size: " + plan.getPartySize());
+            System.out.println("Food Type: " + plan.getFoodType());
+            System.out.println("Beverage Type: " + plan.getBeverageType());
+            System.out.println("Entertainment Type: " + plan.getEntertainmentType());
+            System.out.println("Decorations Type: " + plan.getDecorationsType());
+            System.out.println("Total Price: " + plan.totalPrice());
+            System.out.println("New Total Price: "+ plan.fiveFiveZeroDiscount());
+            System.out.println(" ");
+          }
+        }
+      } else if (userInput.equals("promotions")) {
+        System.out.println("===================");
+        System.out.println("Birthday Decoration Package:");
+        System.out.println("More than 50 guests, dinner, any beverage, clown, birthday decorations");
+        System.out.println("Free birthday decorations");
+        System.out.println("Discount Code: birthday");
+        System.out.println(" ");
+        System.out.println("Wedding Band Package:");
+        System.out.println("More than 100 guests, dinner, premium bar, wedding decorations, band");
+        System.out.println("Free band");
+        System.out.println("Discount Code: wedding");
+        System.out.println(" ");
+        System.out.println("Comedian Sport Package:");
+        System.out.println("More than 50 guests, dinner, any beverage, sports decorations, comedian");
+        System.out.println("Free comedian");
+        System.out.println("Discount Code: sports");
+        System.out.println(" ");
+        System.out.println("$550 off of $2000 or more:");
+        System.out.println("Discount Code: fivefive0");
+        System.out.println(" ");
+        System.out.println("===================");
       } else if(userInput.equals("exit")){
         programRun = false;
       }
